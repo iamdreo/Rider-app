395f3a8ab3f3fcf7d64c75679792c61b87e6e39a
Andrew Ole <34664434+iamdreo@users.noreply.github.com>
2018-11-29 20:22:08 +0100

    Delete PayPalConfig.java

--- java/com/example/android/careapp/PayPalConfig.java
+++
@@ -1,9 +0,0 @@
-package com.example.android.careapp;
-
-/**
- * Created by Dre on 2/7/2018.
- */
-
-class PayPalConfig {
-    public static final String PAYPAL_CLIENT_ID = "AXdKUNzUGJC7PIdn15rVMFf7M9LIWOiH0wEG_tq4pyZZ1yOgVK5ALeQbFsB973cPlhwWa6NiVpNds45A";
-}
